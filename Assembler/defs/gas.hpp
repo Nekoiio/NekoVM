60cabab9813ed3806d4d44ffeffdb3cf67e5ad16
Nekoiio <dguille2023@gmail.com>
2026-09-05 22:06:03 -0400

    Finnished rough prototype of assmebler allows for simple programs

--- Assembler/defs/gas.hpp
+++ Assembler/defs/gas.hpp
@@ -2,9 +2,9 @@
 
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <stdint.h>
 #include <sstream>
+#include <algorithm>
 #include "Instructs/instructions.hpp"
 
 struct func
@@ -14,20 +14,35 @@ struct func
     uint16_t bsize;
 };
 
+struct b2w
+{
+    char bytes[4];
+    int active;
+};
+
 class GAS
 {
     private:
-        func functionStartLocs[100];
+        std::unordered_map<std::string, uint16_t> functionStartLocs;
         uint8_t functionCount = 0;
 
-        uint16_t labelStartLocs[100];
+        std::unordered_map<std::string, uint16_t> labelStartLocs;
         uint8_t labelCount = 0;
 
         uint16_t gradualOff = 0;
 
+        b2w hVariants(
+            std::string& instruction,
+            std::string& operand1,
+            std::string& operand2
+        );
+        void hJmps(std::string& HOI); // Hex Or Int
+        void hCalls(std::string& HOI);
+        int numParse(const std::string& op2, size_t* numlen, char& type);
+        char numType(std::string& op2);
     public:
 
         std::string load(const std::string& filename); 
-        void parse(const std::string& filename);
+        void parse(const std::string& filename, const std::string outfilename);
         void readInstructions(std::ifstream& file, std::ofstream& outFile);
 };
